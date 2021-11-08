f121426902226b2d44e3dd71908e89c050f6a3ec
hanteagyu <snowone4426@gmail.com>
2021-11-08 17:34:46 +0900

    ADD: create project template Changes  - finish client initial settings  - create server package.json

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import GlobalStyles from './style/GlobalStyles'
+
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById("root")
+);
