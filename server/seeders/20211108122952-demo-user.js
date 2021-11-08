7d4d71c9e689b2fdd38020f55d44251c92b75611
he2mo <sivakim91@gmail.com>
2021-11-08 21:53:04 +0900

    ADD: User model & demouser seeder & login api Changes  - created User model, demouser seed, login api Additional  - make mysql database anyname  - type your mysql env_variables  - do migrations model & seed  - npx sequelize-cli db:migrate  - npx sequelize-cli db:seed:all  - run the server & check API  - http://localhost:4000/users/signin  - req.body {"email":"demouser@nbbang.com","password":"1234"}

---
+++ server/seeders/20211108122952-demo-user.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return await queryInterface.bulkInsert('Users', [{
+      username: "demouser",
+      password: "1234",
+      email: "demouser@nbbang.com",
+      profile: null,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return await queryInterface.bulkDelete('Users', null, {});
+  }
+};
