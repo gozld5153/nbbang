55ce04d61ae2ab706246889d932754b7fcc27681
he2mo <sivakim91@gmail.com>
2021-11-10 10:03:45 +0900

    MODIFY: 11/10 최신화 Changes  - dev branch 11/10 시작점

--- server/router/project_router.js
+++ server/router/project_router.js
@@ -1,12 +1,13 @@
-const { signin } = require("../controllers");
+const { project_current, get_project } = require("../controllers");
 const express = require("express");
 const router = express.Router();
 
+router.get("/project/:project_id", get_project);
+// router.get("/current/:id", project_current);
 // app.get("/project/current/:id", controllers.project_current);
 // app.get("/project/complete/:id", controllers.complete_project);
 // app.delete("/project", controllers.delete_project);
 
-// app.get("/project/:project_id", controllers.get_project);
 // app.post("/project", controllers.make_new_project);
 // app.delete("/project/:project_id", controllers.delete_project);
 // app.put("/project/:project_id", controllers.update_project);
