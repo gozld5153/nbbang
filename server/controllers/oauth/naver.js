66c4d2b6f2c22d270b3f7c299702aa8e7c410a0c
he2mo <sivakim91@gmail.com>
2021-11-17 13:11:27 +0900

    ADD: oauth/naver.js Changes  - oauth naver start

---
+++ server/controllers/oauth/naver.js
@@ -0,0 +1,7 @@
+const { Project } = require("../../models");
+
+module.exports = async (req, res) => {
+  // TODO
+  // return res.status(400).json({ data: null, message: "잘못된 요청입니다." });
+  return res.status(500).json({ data: null, message: "아직 구현 안함" });
+};
